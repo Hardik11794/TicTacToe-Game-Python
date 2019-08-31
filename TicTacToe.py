37cea6b4edfbd1f67fe5bdf75a108689f197cb8f
hardik11794@gmail.com <hardik11794@gmail.com>
2019-08-30 22:58:49 -0400

    Made functions for the place the marker , take the input from the user

--- TicTacToe.py
+++ TicTacToe.py
@@ -2,8 +2,12 @@
 print("This is TicTacToe")
 print(' ')
 
-print_board = ['#','O','X','0','X','O','X','O','X','O']
+#print_board = ['#','O','X','0','X','O','X','O','X','O']
+print_board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
 
+#*********************************************************
+#           This function print a board
+#*********************************************************
 def display_board(board):
     print(board[7],'|',board[8],'|',board[9])
     print(board[4],'|',board[5],'|',board[6])
@@ -11,15 +15,54 @@ def display_board(board):
 
 
 display_board(print_board)
+print( ' ')
 
-print(' ')
+#*********************************************************
+#           This function choose player and marker
+#*********************************************************
 
-player_num = int(input("Select player number 1 or 2: "))
-player_sign = input('Choose X or O sign: ')
+def player_input():
+    
+    marker = ' '
 
-if player_num == '1'and player_sign == 'X':
-    First_player = player_num
-    First_Sign = 'X'
-    Second_player = ''
-    Second_Sign = 'O'
+    while marker != 'X' and marker != 'O':
+        marker = input("Player 1 Choose your marker 'X' or 'O' : ").upper()
 
+    player1 = marker
+
+    if player1 == 'X':
+          player2 = 'O'
+    else:
+        player2 = 'X'
+    return(player1,player2)
+
+player_input()
+#*********************************************************
+#           This function place a marker on board
+#*********************************************************
+def place_marker(board,marker,position):
+
+    board.insert(position,marker)
+    display_board(board)
+
+    return(board)
+
+#*********************************************************
+#           This function checks win
+#*********************************************************
+
+def win_check(board,marker):
+
+    if board[1]==board[5]==board[9]== marker or board[3]==board[5]==board[7]== marker or board[1]==board[2]==board[3]== marker or board[4]==board[5]==board[6]== marker or board[7]==board[8]==board[9]== marker or board[7]==board[4]==board[1]== marker or board[8]==board[5]==board[2]== marker or board[9]==board[6]==board[3]== marker:
+        print(f'{marker} You Win,Congratulations !!!')
+        return True
+    else:
+        return False
+
+
+place_marker(print_board,'X',1)
+win_check(print_board,'X')
+place_marker(print_board,'X',5)
+win_check(print_board,'X')
+place_marker(print_board,'X',9)
+win_check(print_board,'X')
