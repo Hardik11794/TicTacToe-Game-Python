5386afe5a9f9a2e1b6fa0866f40dedc06cb3bc4b
hardik11794@gmail.com <hardik11794@gmail.com>
2019-09-23 22:50:31 -0400

    Error Handling added

--- Modules/Choose_Player_Marker.py
+++ Modules/Choose_Player_Marker.py
@@ -1,12 +1,4 @@
-from Modules import Display_Board
-from Modules import Choose_Player_Marker
-from Modules import Place_Marker
-from Modules import Win_Check
-from Modules import Choose_First
-from Modules import Check_Free_Space
-from Modules import Full_Board_Check
-from Modules import Player_Choice
-from Modules import Replay
+
 
 #*********************************************************
 #           This function choose player and marker
--- Modules/Player_Choice.py
+++ Modules/Player_Choice.py
@@ -1,12 +1,9 @@
 from Modules import Display_Board
 from Modules import Choose_Player_Marker
 from Modules import Place_Marker
-from Modules import Win_Check
-from Modules import Choose_First
+
 from Modules import Check_Free_Space
-from Modules import Full_Board_Check
-from Modules import Player_Choice
-from Modules import Replay
+
 
 #********************************************************************************
 #           This function ask player for position and checks if it is available
@@ -16,10 +13,22 @@ from Modules import Replay
 def player_choice(board):
 
     while True:
-        position = int(input("Enter your next position(1-9) : ")) 
-        if Check_Free_Space.space_check(board,position) == True:
-            return int(position)
-            break
-        else:
-            print('This position has been filled up, Choose another one.')
-            continue
+       try:
+             while True:
+                position = int(input("Enter your next position(1-9) : ")) 
+                if position >=1 and position <=10:
+                    break
+                else:
+                    print("\nValue must be between 1 - 9 \n")
+           
+       except:
+           print("\nEnter only integer 1- 9 number \n")
+       else:
+              
+              
+              if Check_Free_Space.space_check(board,position) == True:
+                  return int(position)
+                  break
+              else:
+                  print('This position has been filled up, Choose another one.')
+                  continue
